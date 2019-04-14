a29807e9d87069f048178aec245034d9015ac898
Krzysztof Szypulski <krzychu700@gmail.com>
2019-04-14 21:42:48 +0200

    Updates

--- precache-manifest.1e5d20d578951715a5fd2a7d7a61c8cd.js
+++ precache-manifest.1e5d20d578951715a5fd2a7d7a61c8cd.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/myPortfolio/static/js/runtime~main.918aa22c.js"
   },
   {
-    "revision": "c82f7a0c2718c79e79f7",
-    "url": "/myPortfolio/static/js/main.e370d19c.chunk.js"
+    "revision": "1f5e606d42c1f4887a19",
+    "url": "/myPortfolio/static/js/main.89c6a573.chunk.js"
   },
   {
     "revision": "e3d58dffddf6c9a6447b",
     "url": "/myPortfolio/static/js/2.d9b21298.chunk.js"
   },
   {
-    "revision": "c82f7a0c2718c79e79f7",
-    "url": "/myPortfolio/static/css/main.b6f94b4b.chunk.css"
+    "revision": "1f5e606d42c1f4887a19",
+    "url": "/myPortfolio/static/css/main.09a10a44.chunk.css"
   },
   {
-    "revision": "1eb695f8f149520dac070a90342b7d96",
+    "revision": "e211555676add60a9bceb629c33dbc9b",
     "url": "/myPortfolio/index.html"
   }
 ];
